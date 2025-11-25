159cc8e64bf580079827d665b1c6e478b1dc68e4
Éric <deck.martins.silva@gmail.com>
2025-11-25 10:21:18 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module eglv.sistemagerenciamentoacervos {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens eglv.sistemagerenciamentoacervos to javafx.fxml;
-
-    exports eglv.sistemagerenciamentoacervos;
-}
