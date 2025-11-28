745bc703e94d258daab3909d1401da0bae9b13a1
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-11-28 19:09:11 -0300

    classe editora

--- Fase 4 - Software/Sistema de Gerenciamento de Acervos/src/main/java/eglv/sistemagerenciamentoacervos/db/DbConnector.java
+++ Fase 4 - Software/Sistema de Gerenciamento de Acervos/src/main/java/eglv/sistemagerenciamentoacervos/db/DbConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DbConnector {
     public static final String URL = "jdbc:mysql://localhost:3306/bdmuseueglv?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     public static final String USER = "root";
-    public static final String PASS = "root";
+    public static final String PASS = "0113MA1404A";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
