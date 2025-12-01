9d808b791e4cd79e8e13bff8f78a4e3a8ea34e0c
Vanessa Cezar <vanessacezar3449@gmail.com>
2025-12-01 02:47:48 -0300

    editar assuntos

--- Fase 4 - Software/Sistema de Gerenciamento de Acervos/src/main/java/eglv/sistemagerenciamentoacervos/dao/AssuntoDAO.java
+++ Fase 4 - Software/Sistema de Gerenciamento de Acervos/src/main/java/eglv/sistemagerenciamentoacervos/dao/AssuntoDAO.java
@@ -17,23 +17,23 @@ public class AssuntoDAO {
             ps.setString(1, a.getDescricao());
             ps.executeUpdate();
             try (ResultSet rs = ps.getGeneratedKeys()) {
-                if (rs.next()) a.setId(rs.getInt(1));
+                if (rs.next()) a.setId_assunto(rs.getInt(1));
             }
         }
     }
     public void atualizar(Assunto a) throws SQLException {
-        if (a.getId() == null) throw new SQLException("ID nulo para atualizar.");
-        final String sql = "UPDATE dbo.assunto SET descricao=? WHERE id=?";
+        if (a.getId_assunto() == null) throw new SQLException("ID nulo para atualizar.");
+        final String sql = "UPDATE dbo.assunto SET descricao=? WHERE id_assunto=?";
         try (Connection conn = DbConnector.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, a.getDescricao());
-            ps.setInt(2, a.getId());
+            ps.setInt(2, a.getId_assunto());
             ps.executeUpdate();
         }
     }
 
     public void excluir(int id) throws SQLException {
-        final String sql = "DELETE FROM dbo.assunto WHERE id=?";
+        final String sql = "DELETE FROM dbo.assunto WHERE id_assunto=?";
         try (Connection conn = DbConnector.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, id);
@@ -41,7 +41,7 @@ public class AssuntoDAO {
         }
     }
     public List<Assunto> listar() throws SQLException {
-        final String sql = "SELECT id, descricao FROM dbo.assunto ORDER BY id DESC";
+        final String sql = "SELECT id_assunto, descricao FROM dbo.assunto ORDER BY id_assunto DESC";
         List<Assunto> lista = new ArrayList<>();
         try (Connection conn = DbConnector.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql);
@@ -51,9 +51,26 @@ public class AssuntoDAO {
         return lista;
     }
 
+    public List<Assunto> buscarPorNome(String descricao) throws SQLException {
+        final String sql = "SELECT id_assunto, descricao FROM dbo.assunto WHERE descricao LIKE ?";
+
+        List<Assunto> lista = new ArrayList<>();
+        try (Connection conn = DbConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, "%" + descricao + "%");
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    lista.add(map(rs));
+                }
+            }
+        }
+
+        return lista;
+    }
     private Assunto map(ResultSet rs) throws SQLException {
         return new Assunto(
-                rs.getInt("id"),
+                rs.getInt("id_assunto"),
                 rs.getString("descricao")
         );
     }
